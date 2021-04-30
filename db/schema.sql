4f346e2c82399f5efbbb54bb01300cf118cfb5f0
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-04-29 22:26:37 -0400

    schema.sql
    I am  USE company_db before CREATE table employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,9 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE database company_db;
-
-USE company_db;
-
 CREATE TABLE department (
     dept_id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(30),
     PRIMARY KEY (dept_id)
 );
-
+USE company_db;
 CREATE TABLE company_role (
     role_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
@@ -17,7 +12,7 @@ CREATE TABLE company_role (
     PRIMARY KEY (role_id),
     FOREIGN KEY (dept_id) REFERENCES department(dept_id)
 );
-
+USE company_db;
 CREATE TABLE employees (
     emp_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
@@ -28,4 +23,5 @@ CREATE TABLE employees (
     FOREIGN KEY (emp_role_id) REFERENCES company_role(role_id),
     FOREIGN KEY (manager_id) REFERENCES employees(emp_id)
 );
+
 
